241790c36af620dce082cddf72c4096d434324c1
kilik42 <marvinevins@yahoo.com>
2021-01-02 04:34:46 -0600

    first migration

---
+++ contracts/DappToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.16;
+
+
+contract DappToken {
+
+
+    //constructor
+
+
+    //set the token total
+
+
+    //read the total number of tokens
+    uint256 public totalSupply;
+
+     constructor () public {
+        totalSupply = 10000000;
+
+    }
+    
+}
